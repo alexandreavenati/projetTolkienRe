b134a71229dfb2a598c3ec9d972ee2c984c6f1ee
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-21 11:09:19 +0100

    Section film LoR

--- view/partial/index-main.php
+++ view/partial/index-main.php
@@ -1,3 +1,4 @@
 <?php 
 require_once('book.php'); 
+require_once('movie.php');
 ?>
--- view/partial/movie.php
+++ view/partial/movie.php
@@ -1,17 +1,17 @@
-<section class="container" id="livre">
-    <h2>Les livres</h2>
+<section class="container" id="film">
+    <h2>Les Films</h2>
 
-    <div id="liCommu">
+    <div id="fiCommu">
         <article>
             <h3>La communauté de l'anneau</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde cum voluptatibus vitae itaque nam, quas, laborum architecto temporibus repudiandae omnis culpa provident amet. Incidunt illum harum, fuga laboriosam tempora reprehenderit!</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aut assumenda distinctio eum quas temporibus deleniti. Placeat laborum aperiam, voluptate ea non, reprehenderit sit beatae ipsam similique doloremque atque et.</p>
         </article>
-        <div><img src="../img/livre1.jpg" alt="Livre : Communauté de l'anneau"></div>
+        <div><img src="../img/film1.jpg" alt="Livre : Communauté de l'anneau"></div>
     </div>
 
-    <div id="liTour">
-        <div><img src="../img/livre2.jpg" alt="Livre : Les deux tours"></div>
+    <div id="fiTour">
+        <div><img src="../img/film2.jpg" alt="Livre : Les deux tours"></div>
         <article>
             <h3>Les deux tours</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, eveniet accusantium modi labore cum molestias? Quae consequatur fuga sunt eum enim consequuntur quisquam ea officia provident nulla, voluptas quis voluptatum?</p>
@@ -19,13 +19,13 @@
         </article>
     </div>
 
-    <div id="liRetour">
+    <div id="fiRetour">
         <article>
             <h3>Le retour du roi</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laborum laudantium, non omnis asperiores veniam. Exercitationem, quia culpa asperiores sint, dolorem placeat eligendi distinctio et maiores repellat enim eveniet doloremque.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laborum laudantium, non omnis asperiores veniam. Exercitationem, quia culpa asperiores sint, dolorem placeat eligendi distinctio et maiores repellat enim eveniet doloremque.</p>
         </article>
-        <div><img src="../img/livre3.jpg" alt="Livre : Le retour du roi"></div>
+        <div><img src="../img/film3.jpg" alt="Livre : Le retour du roi"></div>
     </div>
 
 </section>
