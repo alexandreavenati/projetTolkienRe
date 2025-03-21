ebcc702e817e03354cd6691fa8e678da0a0a232e
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-21 11:45:14 +0100

    Media LoR

--- view/partial/index-main.php
+++ view/partial/index-main.php
@@ -1,4 +1,5 @@
 <?php 
 require_once('book.php'); 
 require_once('movie.php');
+require_once('media.php');
 ?>
---
+++ view/partial/media.php
@@ -0,0 +1,36 @@
+<section class="container" id="media">
+    <h2>Autres médias</h2>
+
+    <section>
+        <div>
+            <h3>Les jeux vidéos</h3>
+            <figure>
+                <div><img src="../img/jeuxvideo1.jpg" alt="Jeux vidéos"></div>
+                <div><img src="../img/jeuxvideo1.jpg" alt="Jeux vidéos"></div>
+            </figure>
+        </div>
+
+        <hr>
+
+        <div>
+            <h3>Les comics</h3>
+            <figure>
+                <div><img src="../img/comics1.jpg" alt="Comics"></div>
+                <div><img src="../img/comics1.jpg" alt="Comics"></div>
+            </figure>
+        </div>
+
+        <hr>
+
+        <div>
+            <h3>Les jouets</h3>
+            <figure>
+                <div><img src="../img/jouet1.jpg" alt="Jouets"></div>
+                <div><img src="../img/jouet1.jpg" alt="Jouets"></div>
+            </figure>
+        </div>
+    </section>
+
+</section>
+
+<hr>
