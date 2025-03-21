b7283ffdf742fcc1cb34610212bb763e5631426c
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-21 10:33:07 +0100

    Header LoR

---
+++ view/partial/index-header.php
@@ -0,0 +1,32 @@
+<section>
+    <div><img src="../img/logo.png" alt="Anneau de LoR"></div>
+
+    <h1>Le seigneur des Anneaux</h1>
+
+    <div><img src="../img/logo.png" alt="Anneau de LoR"></div>
+</section>
+
+<div>
+    <nav>
+        <ul>
+            <li><a href="#livre">Les Livres</a>
+                <ul>
+                    <li><a href="#liCommu">La communauté de l'anneau</a></li>
+                    <li><a href="#liTour">Les deux tours</a></li>
+                    <li><a href="#liRetour">Le retour du roi</a></li>
+                </ul>
+            </li>
+            <li><a href="#film">Les Films</a>
+                <ul>
+                    <li><a href="#fiCommu">La communauté de l'anneau</a></li>
+                    <li><a href="#fiTour">Les deux tours</a></li>
+                    <li><a href="#fiRetour">Le retour du roi</a></li>
+                </ul>
+            </li>
+            <li><a href="#media">Autres médias</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+</div>
+
+<hr>
