8befb2222f09358d44c014b0808911ae4ab7c8d9
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-21 12:02:29 +0100

    contact LoR

---
+++ view/partial/contact.php
@@ -0,0 +1,39 @@
+<section class="container" id="contact">
+
+    <form action="">
+        <div>
+            <label for="nom">Entrez votre nom</label> <br>
+            <input type="text" name="nom_" id="nom" placeholder="Votre nom" required>
+        </div>
+
+        <br>
+
+        <div>
+            <label for="prenom">Entrez votre prénom</label> <br>
+            <input type="text" name="prenom_" id="prenom" placeholder="Votre prénom" required>
+        </div>
+
+        <br>
+
+        <div>
+            <label for="mail">Entrez votre mail</label> <br>
+            <input type="email" name="email" id="mail" placeholder="Votre mail" required>
+        </div>
+
+        <br>
+
+        <div>
+            <label for="msg">Entrez votre message</label> <br>
+            <textarea rows="8" cols="50" placeholder="Votre message" name="message" id="msg" required></textarea> <br>
+        </div>
+
+        <br>
+
+        <div>
+            <input type="submit" value="Envoyer">
+        </div>
+    </form>
+
+</section>
+
+<hr>
--- view/partial/index-main.php
+++ view/partial/index-main.php
@@ -2,4 +2,5 @@
 require_once('book.php'); 
 require_once('movie.php');
 require_once('media.php');
+require_once('contact.php');
 ?>
