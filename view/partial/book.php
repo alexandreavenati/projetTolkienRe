a7c263e1c11fe8fafd687392efc7cfdb77b6e257
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-21 11:05:02 +0100

    LoR section livres

---
+++ view/partial/book.php
@@ -0,0 +1,33 @@
+<section class="container" id="livre">
+    <h2>Les livres</h2>
+
+    <div id="liCommu">
+        <article>
+            <h3>La communauté de l'anneau</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde cum voluptatibus vitae itaque nam, quas, laborum architecto temporibus repudiandae omnis culpa provident amet. Incidunt illum harum, fuga laboriosam tempora reprehenderit!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate aut assumenda distinctio eum quas temporibus deleniti. Placeat laborum aperiam, voluptate ea non, reprehenderit sit beatae ipsam similique doloremque atque et.</p>
+        </article>
+        <div><img src="../img/livre1.jpg" alt="Livre : Communauté de l'anneau"></div>
+    </div>
+
+    <div>
+        <div><img src="../img/livre2.jpg" alt="Livre : Les deux tours"></div>
+        <article>
+            <h3>Les deux tours</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, eveniet accusantium modi labore cum molestias? Quae consequatur fuga sunt eum enim consequuntur quisquam ea officia provident nulla, voluptas quis voluptatum?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, eveniet accusantium modi labore cum molestias? Quae consequatur fuga sunt eum enim consequuntur quisquam ea officia provident nulla, voluptas quis voluptatum?</p>
+        </article>
+    </div>
+
+    <div>
+        <article>
+            <h3>Le retour du roi</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laborum laudantium, non omnis asperiores veniam. Exercitationem, quia culpa asperiores sint, dolorem placeat eligendi distinctio et maiores repellat enim eveniet doloremque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laborum laudantium, non omnis asperiores veniam. Exercitationem, quia culpa asperiores sint, dolorem placeat eligendi distinctio et maiores repellat enim eveniet doloremque.</p>
+        </article>
+        <div><img src="../img/livre3.jpg" alt="Livre : Le retour du roi"></div>
+    </div>
+
+</section>
+
+<hr>
